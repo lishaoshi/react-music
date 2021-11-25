501b9fc72a8766ce3ec5dea323270c3648f45a5c
lishaoshi <1332456550@qq.com>
2021-11-25 13:59:27 +0800

    使用css modules切兼用antd样式

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,34 @@
+import Style from './index.module.scss'
+import { Layout, Button } from 'antd';
+
+const { Header, Content, Sider } = Layout;
+
+
+
+
+const Head = () => {
+  return <Header  className={ Style.header }>
+    header
+  </Header>
+}
+
+const HomeContent = () => {
+  return <Layout className={ Style.content }>
+    <Sider className={Style.siderBar}  width={200}>sider</Sider>
+    <Main />
+  </Layout>
+}
+
+const Main = () => {
+  return <Layout>
+    <Content>Main </Content>
+  </Layout>
+}
+
+const Home = () => {
+  return <Layout>
+    <Head />
+    <HomeContent />
+  </Layout>
+}
+export default Home
