64fe99567f188a166a902b8dd34e41204e4f9049
lss <1332456550@qq.com>
2021-11-25 23:25:53 +0800

    add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
-    parser: '@typescript-eslint/parser', // 定义ESLint的解析器
-    // extends: ['plugin:prettier/recommended'], //定义文件继承的子规范
+    // parser: '@typescript-eslint/parser', // 定义ESLint的解析器
+    extends: ['react-app', 'react-app/jest', 'prettier'], //定义文件继承的子规范
     // plugins: ['@typescript-eslint', 'react-hooks'], //定义了该eslint文件所依赖的插件
     // env: {
     //   //指定代码的运行环境
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -30,5 +30,5 @@ const Home = () => {
     <HomeContent />
   </Layout>
 }
-
+  
 export default Home
